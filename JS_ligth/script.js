806480ecf9b4f8be96b750d3f7e6a70b7fecf2f2
olgaKhristo <olga84h@gmail.com>
2022-10-24 14:31:28 +0100

    strt

---
+++ JS_ligth/script.js
@@ -0,0 +1,6 @@
+function turnOn (){
+    document.getElementById ("img-bulb").src= "./img/pic_bulbon.gif"
+}
+function turnOff() {
+    document.getElementById("img-bulb").src = "./img/pic_bulboff.gif"
+}
