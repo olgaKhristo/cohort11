d8ee34554e4d4718de340387cf80ddba85b542d7
olgaKhristo <olga84h@gmail.com>
2022-11-02 22:59:42 +0000

    add AM&PM

--- Focus_extention/script.js
+++ Focus_extention/script.js
@@ -4,6 +4,8 @@ const time = document.getElementById('time'),
 greeting = document.getElementById('greeting'),
 name = document.getElementById('name'),
 focus = document.getElementById('focus');
+//option to see AM or PM
+const showAmPm = true;
 //fanction to show the time
 function showTime(){
     let today = new Date(),
@@ -15,7 +17,7 @@ function showTime(){
     //12 hours format
     hour = hour % 12 || 12;
 //output
-time.innerHTML = `${hour}<span>:</span> ${addZero(min)}<span>:</span> ${addZero(sec)}`;
+time.innerHTML = `${hour}<span>:</span> ${addZero(min)}<span>:</span> ${addZero(sec)} ${showAmPm ? amPm : ''}`;
 setTimeout(showTime, 1000); 
 }
 //add Zero
@@ -54,15 +56,17 @@ function getName() {
 
 //set name
 function setName(e) {
-if (e.type === 'keypress') {
-    //make sure enter is pressed
-    if (e.which == 13 || e.keyCode == 13) {
-        localStorage.setItem('name' e.target.innerText);
-        name.blur();        
-    } 
-} else { localStorage.setItem('name' e.target.innerText); 
+    if(e.type === 'keypress') {
+//make sure enter is pressed
+      if(e.which == 13 || e.keyCode == 13) {
+          localStorage.setItem('name', e.target.innerText);
+          name.blur();
 }
+    } else {
+        localStorage.setItem('name', e.target.innerText);
+    }
 }
+
 
 //get focus
 function getFocus() {
@@ -73,8 +77,23 @@ function getFocus() {
     }
 }
 
+//set focus
+function setFocus(e) {
+    if (e.type === 'keypress') {
+        //make sure enter is pressed
+        if (e.which == 13 || e.keyCode == 13) {
+            localStorage.setItem('focus', e.target.innerText);
+            focus.blur();
+        }
+    } else {
+        localStorage.setItem('focus', e.target.innerText);
+    }
+}
+
 name.addEventListener('keypress', setName);
 name.addEventListener('blur', setName);
+focus.addEventListener('keypress', setFocus);
+focus.addEventListener('blur', setFocus);
 //run
 showTime();
 setBgGreet();
