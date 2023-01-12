992e4fc923fe8f74c863e4599f31586b74f20e10
olgaKhristo <olga84h@gmail.com>
2023-01-12 23:21:35 +0000

    all

---
+++ JSbanana/script.js
@@ -0,0 +1,11 @@
+
+function letterCount(letter,word){
+    let count = 0;
+    for (let i = 0; i < word.length; i++){
+    if (word[i] == letter){
+        count++;
+       
+    }}
+    console.log(count);
+}
+letterCount('a','banana')
